fdc5baab73e3b48bf21bbf8f032d06643bf3a539
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-05-31 16:05:11 +0200

    Script: Add fwd sw script

---
+++ scripts/linear_model_forward_steps.R
@@ -0,0 +1,33 @@
+bestProb = 1
+#already_present = c(already_present,42)
+
+for(i in 2:length(columns)) {
+  if(columns[i] %in% already_present)
+    next
+  myFrame = train[,c(already_present,columns[i])]
+  train.lm2 = lm(train$count ~ .,data = myFrame)
+  
+  myAnova = anova(train.lm,train.lm2)
+  print(columns[i])
+  str(myAnova$`Pr(>F)`)
+  if(myAnova$`Pr(>F)`[2] < bestProb) {
+    bestProb = myAnova$`Pr(>F)`[2]
+    best = i
+  }
+}
+
+if(bestProb < FWD_SW_THRESHOLD) {
+  already_present = c(already_present,columns[best])
+  myFrame = train[,c(already_present)]
+  train.lm = lm(train$count ~ .,data = myFrame)
+  print(paste(columns[best], "column has been added to our linear model"))
+  source("scripts/linear_model_forward_steps.R")
+}
+
+#clean
+if(exists("myFrame"))
+  rm(myFrame)
+if(exists("myAnova"))
+  rm(myAnova)
+if(exists("bestProb"))
+  rm(bestProb)
