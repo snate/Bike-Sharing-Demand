fe3b890619cbd960a0308813e140f441d6c4d25c
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-06-07 14:00:23 +0200

    Script: Express weather as quantitative

--- scripts/populate.R
+++ scripts/populate.R
@@ -27,25 +27,6 @@ train$season[train$season == 2] = 0
 train$season[train$season == 3] = 0
 train$season[train$season == 4] = 0
 
-#WEATHER#
-# decent
-train$weather.decent = rep(0,length(train$weather))
-train$weather.decent[train$weather == 2] = 1
-test$weather.decent = rep(0,length(test$weather))
-test$weather.decent[test$weather == 2] = 1
-# bad
-train$weather.bad = rep(0,length(train$weather))
-train$weather.bad[train$weather == 3] = 1
-test$weather.bad = rep(0,length(test$weather))
-test$weather.bad[test$weather == 3] = 1
-# bring all to 0 except good
-test$weather[test$weather == 2] = 0
-test$weather[test$weather == 3] = 0
-test$weather[test$weather == 4] = 0
-train$weather[train$weather == 2] = 0
-train$weather[train$weather == 3] = 0
-train$weather[train$weather == 4] = 0
-
 # COSTANTS
 FWD_SW_THRESHOLD = 0.05
 MAX_P_DEGREE = 30
