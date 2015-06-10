16dcbc2792902e0c46a7090f207e6d191ac07316
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-06-10 19:43:59 +0200

    Script: Migliora disegno smoothing

--- scripts/drawOptimalSM_datetime.R
+++ scripts/drawOptimalSM_datetime.R
@@ -1,5 +1,5 @@
 x = train$datetime
-y = train$count
+y = log(train$count)
 
 plot(x,y,col=3,pch=3)
 sm1 = sm.regression(x,y,h=optimal,add=TRUE,ngrid=120,col=2)
---
+++ scripts/drawOptimalSM_temp.R
@@ -0,0 +1,9 @@
+x = train$temp
+y = log(train$count)
+
+plot(x,y,col=3,pch=3)
+sm1 = sm.regression(x,y,h=optimal,add=TRUE,ngrid=120,col=2)
+
+rm(sm1)
+rm(x)
+rm(y)
