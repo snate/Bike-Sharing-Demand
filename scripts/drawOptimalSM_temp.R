711a4d57ec7145072390d4bce4d88612b59724b3
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-06-12 21:37:53 +0200

    Script: Usa h ottimo calcolato con script mySM*

--- scripts/drawOptimalSM_temp.R
+++ scripts/drawOptimalSM_temp.R
@@ -2,7 +2,7 @@ x = train$temp
 y = log(train$count)
 
 plot(x,y,col=3,pch=3)
-sm1 = sm.regression(x,y,h=optimal,add=TRUE,ngrid=120,col=2)
+sm1 = sm.regression(x,y,h=optimal_h,add=TRUE,ngrid=120,col=2)
 
 rm(sm1)
 rm(x)
