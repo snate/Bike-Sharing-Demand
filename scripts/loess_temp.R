2528d6569e166055d0eda74a5e1aa656f4e97d1e
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-06-14 12:01:06 +0200

    Script: Several corrections to loess_temp.R

--- scripts/loess_temp.R
+++ scripts/loess_temp.R
@@ -2,11 +2,11 @@ indexes = sample(1:length(train$count), size=length(train$count)/2)
 indexes.v = sample(setdiff(1:length(train$count), indexes))
 str(indexes)
 str(indexes.v)
+column = "temp"
 
-x = train$temp
-str(x)
-x.v = x[indexes.v]
-x = x[indexes]
+x = train[column]
+x.v = x[indexes.v,column]
+x = x[indexes,column]
 y = log(train$count)
 y.v = y[indexes.v]
 y = y[indexes]
@@ -22,11 +22,12 @@ for(i in nummin:num) {
   val[i,2]<-sum((y.v-predict(lo1))^2)
 }
 
-plot(val[nummin:num,1],val[nummin:num,2],xlab="parametro di lisciamento",ylab="errore 'dati di oggi'")
+plot(val[nummin:num,1],val[nummin:num,2],xlab="Span",ylab="Errore")
 print(val[nummin:num,])
 optimal_span = min(val[,2])
 optimal_span = val[,1][val[,2] == optimal_span]
 print(paste("Optimal span is",optimal_span))
+cat("premere <Enter>"); readline()
 
 lo1 <- loess.smooth(x,y,span=optimal_span)
 plot(x,y,pch=2,col=2,main="loess")
