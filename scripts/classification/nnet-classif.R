4102bc7264b6ed0845cf59636f41392e60673004
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-06-20 17:08:34 +0200

    Script: Better formatting for class-tree & class-nnet

--- scripts/classification/nnet-classif.R
+++ scripts/classification/nnet-classif.R
@@ -1,28 +1,30 @@
+# NEURAL NETWORKS - CLASSIFICATION #
+# Load library 'nnet' and graphic tools
 library(nnet)
+library(devtools)
+source_url('https://gist.githubusercontent.com/fawda123/7471137/raw/466c1474d0a505ff044412703516c34f1a4684a5/nnet_plot_update.r')
 
-FN = as.formula(paste("aLotRegistered~",paste(names(train[-c(10:12,15)]), collapse="+")))
+# Set formula to compute neural network
+FN = as.formula(paste("aLotCasual~",paste(names(train[-c(10:12,15)]), collapse="+")))
+parte1 = sample(1:NROW(train), 8000) 
+parte2 = setdiff(1:NROW(train), parte1)
 
-parte1<- sample(1:NROW(train), 8000) 
-parte2<- setdiff(1:NROW(train), parte1)
-
-#---
-#selezione del weight decay
-#ci mette molto tempo
-#---
-decay<- 10^(seq(-3, -1, length=10))
-err <- rep(0,10)
+# Find best decay for neural network
+decay = 10^(seq(-3, -1, length=10))
+err = rep(0,10)
 for(k in 1:10){
-  n1<- nnet(FN, data=train[parte1,], decay=decay[k], size=5,
+  n1 = nnet(FN, data=train[parte1,], decay=decay[k], size=5,
             maxit=100,  trace=FALSE)
-  p1n<- predict(n1, newdata=train[parte2,], type="class")
-  a = tabella.sommario(p1n, train[parte2,]$aLotRegistered)
-  err[k] <-  1-sum(diag(a))/sum(a)
+  p1n = predict(n1, newdata=train[parte2,], type="class")
+  a = tabella.sommario(p1n, train[parte2,]$aLotCasual)
+  err[k] =  1-sum(diag(a))/sum(a)
   print(c(err[k], decay[k]))
 }
-#
-#
-n2<- nnet(FN, data=train[parte1,], decay=min(decay), size=5,  maxit=1000)
-p7<- predict(n1, newdata=x.v, type="class")
-#
+
+# Compute a neural network with the best decay found in previous step
+n2 = nnet(FN, data=train[parte1,], decay=min(decay), size=5,  maxit=1000)
+pNET = predict(n1, newdata=x.v, type="class")
+
+# Draw classification results
 tabella.sommario(p7,y.v)
-a7<- lift.roc(p7, aLotRegisteredAsNumber, type="crude")
+a7 = lift.roc(p7, aLotCasualAsNumber, type="crude")
--- scripts/classification/tree-classif.R
+++ scripts/classification/tree-classif.R
@@ -1,30 +1,37 @@
-# TREE #
+# TREE - CLASSIFICATION #
+# Load library 'tree'
 library(tree)
 
+# Convert hours to numbers
 train$datetime = as.numeric(train$datetime)
 x.v$datetime = as.numeric(x.v$datetime)
 
-FT = as.formula(paste("aLotRegistered~",paste(names(train[-c(10:12,15)]), collapse="+")))
+# Set formula to compute neural network
+FT = as.formula(paste("aLotCasual~",paste(names(train[-c(10:12,15)]), collapse="+")))
 
+# Find whole tree
 parte1<- sample(1:NROW(train), 8000) 
 parte2<- setdiff(1:NROW(train), parte1)
 #
-t1b<- tree(FT, data=train[parte1,], control=tree.control(nobs=length(parte1), minsize=2, mindev=0.01))
-plot(t1b)
-text(t1b,cex=0.8)
+t1c<- tree(FT, data=train[parte1,], control=tree.control(nobs=length(parte1), minsize=2, mindev=0.01))
+plot(t1c)
+text(t1c,cex=0.8)
 cat("press <Enter>"); readline()
 
-t2b<- prune.tree(t1b, newdata=train[parte2,])
-plot(t2b)
-J <- t2b$size[t2b$dev==min(t2b$dev)]
+# Find deviance for all the subtrees of t1
+t2c<- prune.tree(t1c, newdata=train[parte2,])
+plot(t2c)
+J <- t2c$size[t2b$dev==min(t2c$dev)]
 cat("press <Enter>"); readline()
-#
-t3b<-prune.tree(t1b, best=J)
-plot(t3b)
-text(t3b)
+
+# Get the tree with best size (and minimum deviance)
+t3c<-prune.tree(t1c, best=J)
+plot(t3c)
+text(t3c)
 cat("press <Enter>"); readline()
-#
-pp5b<- predict(t3b, newdata=x.v, type="class")
-tabella.sommario(pp5b,y.v)
-p5b <-  predict(t3b, newdata=x.v, type="vector")[,2]
-a5b <- lift.roc(p5b,aLotRegisteredAsNumber,type="crude")
+
+# 
+pTREEb = predict(t3c, newdata=x.v, type="class")
+tabella.sommario(pTREEb,y.v)
+pTREE <-  predict(t3c, newdata=x.v, type="vector")[,2]
+atree <- lift.roc(pTREE,aLotCasualAsNumber,type="crude")
