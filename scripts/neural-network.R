b9746211f0e7dc1cf5a22790150276892eb5cf33
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-06-20 15:27:56 +0200

    Script: Add library usage in nnet

--- scripts/neural-network.R
+++ scripts/neural-network.R
@@ -1,5 +1,8 @@
 #RETI NEURALI
 library(nnet)
+library(devtools)
+source_url('https://gist.githubusercontent.com/fawda123/7471137/raw/466c1474d0a505ff044412703516c34f1a4684a5/nnet_plot_update.r')
+
 Fnet = as.formula(paste("count~",paste(names(train[-c(10:12)]), collapse="+")))
 indexes = sample(1:length(train$count), size=length(train$count)/2 + 1)
 indexes.v = sample(setdiff(1:length(train$count), indexes))
