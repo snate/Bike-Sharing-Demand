249eb78974e1364686104b4a92ed85f856c2fd93
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-06-20 15:28:11 +0200

    Script: Several changes

--- scripts/linearModel.R
+++ scripts/linearModel.R
@@ -2,10 +2,21 @@ if(!is.factor(train$datetime))
   train$datetime = factor(train$datetime)
 r2 = rep(0,length(columns)); fstatistic = rep(0,length(columns))
 
+indexes = sample(1:length(train$count), size=length(train$count)/2)
+indexes.v = sample(setdiff(1:length(train$count), indexes))
+
+x = train[columns]
+y = log(train$count)
+x.v = x[indexes.v,]
+x = x[indexes,]
+y.v = y[indexes.v]
+y = y[indexes]
+
+
 for(i in 1:length(columns)) {
   print(columns[i])
-  myCol = train[columns[i]]
-  train.lm = lm(log(train$count)~., data = myCol)
+  myCol = x[columns[i]]
+  train.lm = lm(y ~ ., data = myCol)
   r2[i] = summary(train.lm)$r.squared
   fstatistic[i] = summary(train.lm)$fstatistic[1]
   print(paste(columns[i], "has R-squared:", r2[i], " and F-statistic: ", fstatistic[i]))
@@ -20,16 +31,25 @@ for(i in 2:length(columns)) {
 print(paste(columns[best], "has best F-statistic:", fstatistic[best]))
 print(paste(columns[best], "will be used to calculate the linear model w/ 1 variable"))
 
-train.lm = lm(log(train$count) ~ ., data = train[columns[best]])
-
+train.lm = lm(y ~ ., data = x[columns[best]])
 already_present = c(columns[best])
 source("scripts/linear_model_forward_steps.R")
 print(already_present)
 
+pLinear = predict(train.lm,x.v)
+aLinear = lift.roc(pLinear, y.v, type="crude")
+
 #clean
-rm(i)
-rm(best)
-rm(r2)
-rm(fstatistic)
 rm(already_present)
+rm(best)
+rm(fstatistic)
+rm(i)
+rm(indexes)
+rm(indexes.v)
 rm(myCol)
+rm(pLinear)
+rm(r2)
+rm(x)
+rm(x.v)
+rm(y)
+rm(y.v)
