b89b447220bfe7971dbfce6fb46d7a6445b920f0
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-06-20 16:35:44 +0200

    Script: Improva class-GAM script

--- scripts/classification/gam-classif.R
+++ scripts/classification/gam-classif.R
@@ -1,20 +1,14 @@
-#------------
-#GAM
-#------------
-# 
+# GAM #
 library(gam)
-#-------
-#esercizio di con poche variabili esplicative, da usare a lezione
-#
 
 gam2 =  gam(F1, family=binomial, data=x)
 pGAM = predict(gam2,newdata=x.v,type="response")
-#
+
 err.gamc2 = tabella.sommario(pGAM>0.5,y.v)
 err.gamc2
-#
+
 summary(gam2)
-a8b = lift.roc(pGAM, aLotRegisteredAsNumber, type="crude")
+aGAM = lift.roc(pGAM, aLotCasualAsNumber, type="crude")
 
 cat("press <Enter>"); readline()
 plot(gam2,ask=TRUE)
